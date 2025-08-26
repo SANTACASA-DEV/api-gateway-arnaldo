208f02cb0c6b2ed20700d454686d1f6644fb4b15
Arnaldo Liro <dinholiro@gmail.com>
2025-08-26 14:25:34 -0300

    first commit

---
+++ src/test/java/br/com/santacasa/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.santacasa.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
